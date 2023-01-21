e7f75f55904ba768dd7c3e5b2378401d788da4ac
desmondlb <desmondlb@gmail.com>
2023-01-21 18:57:18 -0500

    Initial

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import { cors } from "cors";
+import { dotenv } from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Configurations
